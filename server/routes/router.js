b51227073123b0c3de45cbc3798bbdaa768a78b1
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-11-11 17:22:37 -0600

    refactored router.js GET and POST

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,44 +1,37 @@
+const {query} = require('express');
 const express = require('express');
 const router = express.Router();
-
-// DB CONNECTION
-const pg = require('pg');
-const Pool = pg.Pool;
-const pool = new Pool ({
-    database: 'todo_list',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
+const pool = require('../modules/pool');
 
 //GET
 router.get('/', (req, res) => {
     let queryText = 'SELECT * FROM todo;';
-    pool.query(queryText).then((result) => {
-        console.log(result.rows);
-        res.send(result.rows);
-    }).catch ((err) => {
+    pool.query(queryText)
+        .then((result) => {
+            console.log(result.rows);
+            res.send(result.rows);
+    })
+    .catch ((err) => {
         console.log('Error making query:', queryText, 'error:', err);
         res.sendStatus(500);
     })
-})
+});
 
 //POST
 router.post('/', (req, res) => {
     let newTodo = req.body;
+
     console.log('adding new todo', newTodo);
+
     let queryText = `INSERT INTO  "todo" ("list_item", "complete")
                         VALUES($1, $2)`;
-    pool.query(queryText, [
-            newTodo.list_item,
-            newTodo.complete
-])
-.then((result) => {
+    pool.query(queryText, [newTodo.list_item, newTodo.complete])
+        .then((result) => {
+            console.log('POST result from DB is:', result);
         res.sendStatus(201);
     })
     .catch((err) => {
-        console.log('error adding new Koala', err);
+        console.log('error POSTing query', queryText, 'error:', err);
         res.sendStatus(500);
     });
 });
